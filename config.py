024805aab3058dd8bd2f6cb2f894bf073edfc03b
Tvkattsu103 <voanhtuan103@gmail.com>
2021-12-17 10:36:55 +0700

    divide prj

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config(object):
+    UPLOAD_FOLDER = 'static/images'
+
+    RECAPTCHA_PUBLIC_KEY = '6LeckacdAAAAAHSQu1A2h2ZXnXSrcoAFwIOvyi3d'
+    RECAPTCHA_PRIVATE_KEY = '6LeckacdAAAAADOPi7qaBle16terydNyysELh9E1'
+    
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/qlloterria'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = 'SECRET_KEY'
---
+++ models.py
@@ -0,0 +1,37 @@
+from App import db, login
+from flask_login import UserMixin
+
+#Creating model table for our CRUD database
+class Food(db.Model):
+    foodid = db.Column(db.Integer, primary_key=True)
+    foodname = db.Column(db.String(100))
+    price = db.Column(db.String(100))
+    description = db.Column(db.String(500))
+    image = db.Column(db.String(100))
+    category = db.Column(db.String(100))
+
+    def __init__(self, foodname, price, description, image, category):
+        self.foodname = foodname
+        self.price = price
+        self.description = description
+        self.image = image
+        self.category = category
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    role = db.Column(db.Integer)
+    
+    def get_id(self):
+        return self.id
+    def is_active(self): 
+        return True
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
